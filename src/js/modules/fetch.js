389b2848e4668d0eef5d42c7b2b557ef3fc0945d
ROEL2407 <roel24komen@gmail.com>
2022-03-16 14:08:53 +0100

    Update: changed fetch

--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -1,14 +1,17 @@
-import {makeItems} from './create.js';
+import {
+    makeItems
+} from './create.js';
+
 
 const main = document.querySelector("main");
 const cors = "https://cors-anywhere.herokuapp.com/";
 const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
-const query = "Kookboeken+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia"; /* Kookboeken&dim=Type(book)&dim=Topic(Kookboeken) */
+const query = "kookboeken+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia"; /* Kookboeken&dim=Type(book)&dim=Topic(Kookboeken) */
 const key = "fff5cd7a65bd87deefd8f70bfb447d42";
 const detail = "Default";
 const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
 
-const display = document.getElementById('itemList');
+export const display = document.getElementById('itemList');
 display.textContent = "Loading...";
 
 const itemArray = [];
@@ -31,9 +34,26 @@ export function getData() {
             })
             renderData();
         })
-        .catch((err) => {
-            console.log(err);
-        });
+
+        .catch(() => {
+            fetch('../src/data.json')
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response);
+                    response.results.forEach(item => {
+                        itemArray.push({
+                            id: item.id,
+                            title: item.titles[1],
+                            author: item.authors,
+                            img: item.coverimages[1],
+                            desc: item.description[0],
+                            link: item.detailLink
+                        });
+                    })
+                    renderData();
+                })
+                .catch(err => console.error(err));
+        })
 }
 
 // render data
