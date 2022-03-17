741782c80941fc5e95f5b7f16d382bd915c46cc9
ROEL2407 <roel24komen@gmail.com>
2022-03-17 13:46:28 +0100

    Update: second api fetch

--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -10,11 +10,12 @@ const query = "kookboeken+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia"; /
 const key = "fff5cd7a65bd87deefd8f70bfb447d42";
 const detail = "Default";
 const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+const url2 = `${cors}http://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=kookboeken+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=fff5cd7a65bd87deefd8f70bfb447d42&output=json`
 
 export const display = document.getElementById('itemList');
 display.textContent = "Loading...";
 
-const SuggestionArray = [];
+export const SuggestionArray = [];
 export const itemArray = [];
 export function getData() {
     fetch(url)
@@ -32,6 +33,11 @@ export function getData() {
                     desc: item.description[0],
                     link: item.detailLink
                 });
+
+                SuggestionArray.push({
+                    title: item.titles[1],
+                    author: item.authors
+                });
             })
             renderData();
         })
@@ -48,12 +54,62 @@ export function getData() {
                             img: item.coverimages[1],
                             desc: item.description[0],
                             link: item.detailLink
-                        })
+                        });
 
                         SuggestionArray.push({
                             title: item.titles[1],
-                            author: item.authors[1]
-                        })
+                            author: item.authors
+                        });
+                    })
+                    console.log(SuggestionArray);
+                    renderData();
+                })
+                .catch(err => console.error(err));
+        })
+}
+export function getData2() {
+    fetch(url2)
+        .then((response) => {
+            return response.json();
+        })
+        .then(response => {
+            console.log(response);
+            response.results.forEach(item => {
+                itemArray.push({
+                    id: item.id,
+                    title: item.titles[1],
+                    author: item.authors,
+                    img: item.coverimages[1],
+                    desc: item.description[0],
+                    link: item.detailLink
+                });
+
+                SuggestionArray.push({
+                    title: item.titles[1],
+                    author: item.authors
+                });
+            })
+            renderData();
+        })
+        .catch(() => {
+            fetch('../src/staging.json')
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response);
+                    response.forEach(item => {
+                        itemArray.push({
+                            id: item.id,
+                            title: item.titles[1],
+                            author: item.authors,
+                            img: item.coverimages[1],
+                            desc: item.description[0],
+                            link: item.detailLink
+                        });
+
+                        SuggestionArray.push({
+                            title: item.titles[1],
+                            author: item.authors
+                        });
                     })
                     console.log(SuggestionArray);
                     renderData();
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
-import {getData} from './modules/fetch.js';
+import {getData, getData2} from './modules/fetch.js';
 import {searchBar} from './modules/search.js';
 
 getData();
+getData2();
 
 document.querySelector("form").addEventListener("submit", searchBar);
