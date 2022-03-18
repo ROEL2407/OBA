33e527a016f3d4b17e42f0c0aaf294b6b610bb1c
ROEL2407 <roel24komen@gmail.com>
2022-03-18 11:42:54 +0100

    Update: prevent words of the screen

--- src/js/modules/create.js
+++ src/js/modules/create.js
@@ -4,7 +4,7 @@ export function makeItems(data) {
         const temp = document.createElement('article');
         temp.setAttribute("class", "book");
         temp.id = item.id;
-        const output = '<img src="' + item.img + '" alt=""><div><h4>' + item.title + '</h4><p>' + item.author + '</p><p>' + item.desc +'</p><p>Categorie(ën):' + item.subject + '</p><a href="' + item.link + '" target="_blank">Meer informatie</a></div>' ;
+        const output = '<img src="' + item.img + '" alt=""><div><h4>' + item.title + '</h4><p>' + item.author + '</p><p>' + item.desc +'</p><p>Categorie(ën): ' + item.subject + '</p><a href="' + item.link + '" target="_blank">Meer informatie</a></div>' ;
         temp.innerHTML = output;
         const itemWrapper = document.getElementById("itemList");
         itemWrapper.appendChild(temp);
